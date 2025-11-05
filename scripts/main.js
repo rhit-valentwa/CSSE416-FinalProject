5f9bb405af1c09826f2742186ed98075fd4ff9b4
Will V <43239509+rhit-valentwa@users.noreply.github.com>
2025-11-05 02:20:08 -0500

    demo

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+(function () {
+  document.querySelectorAll('a[href^="#"]').forEach(a => {
+    a.addEventListener('click', function (e) {
+      e.preventDefault();
+      const t = document.querySelector(this.getAttribute('href'));
+      if (t) t.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+  });
+})();
